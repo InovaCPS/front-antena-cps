0751c19c2aac6f0e10d4ff40433f9c8eb2c8bd5d
RENAN LORENZETTI <lorenzetti2012@gmail.com>
2018-11-01 13:54:48 -0300

    Correção Navbar

--- FRONT/src/components/ToolbarLand/Toolbar.js
+++ FRONT/src/components/ToolbarLand/Toolbar.js
@@ -1,39 +1,27 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'mdbreact';
+import { Navbar, NavbarBrand, NavbarNav, NavItem } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './Toolbar.css';
 
 class ToolbarLand extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collapse: false,
-            isWideEnough: false,
-        };
-    this.onClick = this.onClick.bind(this);
-    }
-
-    onClick(){
-        this.setState({
-            collapse: !this.state.collapse,
-        });
-    }
     render() {
         return (
             <Router>
-                <Navbar color="danger-color-dark" light expand="md" scrolling className="d-flex justify-content-between">
-                    <NavbarBrand href="/">                   
-                            <div className="logo p-2 justify-content-start">
-                                <a href="/">Antena <i class="fas fa-broadcast-tower"></i> CPS</a>
-                            </div>
-                            <div className="p-2 justify-content-end">
-                                <input className="login" placeholder="Usuario" />
-                                <input className="login" placeholder="Senha" />
-                                <button type="login">Entrar</button>
-                            </div>              
-            </NavbarBrand>      
-         </Navbar>
-    </Router>   
+                <Navbar color="danger-color-dark" expand="md" scrolling>
+                    <NavbarBrand className="Logo">
+                        <a href="/">Antena <i class="fas fa-broadcast-tower"></i> CPS</a>
+                    </NavbarBrand>
+                        <NavbarNav right>
+                          <NavItem>
+                            <form className="login">
+                                <input className="item" placeholder="Email" />
+                                <input className="item" placeholder="Senha" />
+                                <button className="item" type="item">Entrar</button>                            
+                            </form>
+                          </NavItem>
+                        </NavbarNav>
+                </Navbar>
+            </Router>
         );
     }
 }
