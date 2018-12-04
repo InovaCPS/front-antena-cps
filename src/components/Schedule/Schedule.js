acfa19a98282030a33c471996e6265ec5d232311
RENAN LORENZETTI <lorenzetti2012@gmail.com>
2018-12-04 13:58:44 -0300

    Finalizando land-page

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,50 +1,62 @@
-import React, { Component }from 'react';
-
+import React, { Component } from 'react';
 import "./Schedule.css";
-import News from '../../components/News/News';
-import FormLand from '../../components/FormLand/FormLand';
 
 class Schedule extends Component{
     state = {
-        titulo: "como programar Orientado a Objeto", 
-	    descricao: "será explicado como programar Orientado a Objeto", 
-	    tipo: "curso", 
-	    duracao: "60", 
-	    banner: "o caminho do banner", 
-	    eventos:[
-		["1", "2018-10-15", "14:00"], 
-		["1", "2018-10-16", "14:00"], 
-		["1", "2018-10-17", "14:00"]
-	], 
-	materiais:[
-		["caminho do material 1"], 
-		["caminho do material 2"]
-	]
-    }
-    onSubmit = (model) => {
-        fetch("http://34.226.121.69:8080/cp/evento",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify(model)
-            })
 
-            .then(response => response.json())
-			.then(data => {
-                this.setState({data: data})
-                return alert( JSON.stringify(data))
-            })
-        
-      }
-    render() {
-        return (
-                <div className="container cont">
-                    
-            </div>
-        );
-      }
     }
+  
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.props.onSubmit) this.props.onSubmit(this.state);
+       
+    }
+  
+    onChange = (e, key) =>{
+        this.setState({
+            [key]: this[key].value
+        })
+    }
+  
+    renderForm = () => {
+        let model = this.props.model;
+  
+        let formUI = model.map((m) => {
+            let key = m.key;
+            let type = m.type || "text";
+            let props = m.props || {};
+  
+            return (
+                
+                <div key={key} className="form-group">
+                    <label className="form-label" key={"l" + m.key} htmlFor={m.label}>{m.label}
+                    </label>
+                    <input {...props}
+                        ref={(key) => {this[m.key]=key}}
+                        className="form-input"
+                        type={type}
+                        key={"i" + m.key}
+                        onChange ={(e)=>{this.onChange(e, key)}} 
+                    />
+                </div>
+            );
+        });
+        return formUI;
+    } 
+    render() {
+      return (
+        <div className="card">
+          <div className="form-wrapper">
+            <h1>Seja um Antenado</h1>
+            <form id="test-form" className="dynamic-form" onSubmit={(e)=>{this.onSubmit(e)}}>
+                  {this.renderForm()}
+                  <div className="form-group">
+                  <button type="submit">ENVIAR</button>
+                  </div>
+            </form>
+          </div>
+        </div>
+      );
+    }
+  }
 export default Schedule;
--- src/pages/anten/anten.js
+++ src/pages/anten/anten.js
@@ -8,7 +8,14 @@ import Schedule from '../../components/Schedule/Schedule';
 
 class anten extends Component{
         state = {
-            sideDrawerOpen: true
+            sideDrawerOpen: true,
+            event : [ {titulo: "como programar Orientado a Objeto", 
+            descricao: "será explicado como programar Orientado a Objeto", 
+            tipo: "curso", 
+            duracao: "60", 
+            banner: "o caminho do banner", 
+            eventos:[""], 
+            materiais:[""]}]
         };
         drawerToggleClickHandler = () =>{
             this.setState((prevState) => {
@@ -30,9 +37,21 @@ class anten extends Component{
                     {sideDrawer}
                     {backdrop}
                 <main className="content">
-                    <Schedule  />
+                    <div className="doc-example">
+                        <Schedule className="form"
+        title = "Registro"
+        model = {[
+            {key: "nome", label: "Nome", props:{}},
+            {key: "sobrenome", label: "Sobrenome", props:{}},
+            {key: "email", label: "Email", props:{}},
+            {key: "senha", label: "Senha", type:"password", props:{min:6, max:12}}
+        ]}        
+        onSubmit = {(model) => {this.onSubmit(model)}}/>
+                    </div>
                 </main>
+                <div className="footer">
                     <Footer />
+                    </div>
                 </div>
             )
         }
