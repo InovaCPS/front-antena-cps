e653c38c4c8f385ec9759988f20cedf0a7a8dadb
RENAN LORENZETTI <lorenzetti2012@gmail.com>
2019-01-23 14:42:01 -0200

    Land Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,46 @@ import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider } from 'angularx-social-login';
+import { ProfileComponent } from './profile/profile.component';
+
+// let config = new AuthServiceConfig([
+//   {
+//     id: GoogleLoginProvider.PROVIDER_ID,
+//     provider: new GoogleLoginProvider("Google-OAuth-Client-Id")
+//   },
+//   {
+//     id: FacebookLoginProvider.PROVIDER_ID,
+//     provider: new FacebookLoginProvider("Facebook-App-Id")
+//   },
+//   {
+//     id: LinkedInLoginProvider.PROVIDER_ID,
+//     provider: new LinkedInLoginProvider("LinkedIn-client-Id", false, 'en_US')
+//   }
+// ]);
+ 
+// export function provideConfig() {
+//   return config;
+// }
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // SocialLoginModule
   ],
-  providers: [],
+  // providers: [    {
+  //   provide: AuthServiceConfig,
+  //   useFactory: provideConfig
+  // }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FacebookService, InitParams} from 'ngx-facebook';
 
 @Component({
   selector: 'app-data-binding',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding.component.css']
 })
 export class DataBindingComponent {
+
+  constructor(private fb: FacebookService) {
+ 
+    let initParams: InitParams = {
+      appId: '1234566778',
+      xfbml: true,
+      version: 'v2.8'
+    };
+ 
+    fb.init(initParams);
+ 
+  }
 
   logo = require('../../app/images/logo1-02.png')
 
