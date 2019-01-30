7f5e9488e80eb88fb221a787a3af7f6f8e0893f8
Renan Lorenzetti <lorenzetti2012@gmail.com>
2019-01-30 13:53:03 -0200

    metodo para realizar o login com a API do Bakc-End e token gerada salva no LocalStorage do Navegador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ContentComponent } from './content/content.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { Component } from "@angular/core";
+import { map } from "rxjs/operators"
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-data-binding',
@@ -6,8 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding.component.css']
 })
 export class DataBindingComponent {
+    apiRoot: string = "http://localhost:8080";
 
-  logo = require('../../app/images/antena_cps.jpg')
-  //logo = require('../../app/images/logo1-02.png')
+    logo = require('../../app/images/antena_cps.jpg')
+    usrn: String;
+    pswd: String;
 
+    constructor(private http: HttpClient) {}
+
+    doPOST() {
+        console.log("POST");
+        let url = `${this.apiRoot}/login`;
+        this.http
+          .post(url, { username: this.usrn, password: this.pswd })
+          .pipe(map(response => { localStorage.setItem('token', JSON.stringify(response)) }))
+          .subscribe(res => console.log("Bem Vindo!!!"))    
+          
+    }
+
+    lgGoogle(){
+        console.log("GET");
+        let url = `${this.apiRoot}/login/google`;
+        this.http
+          .get(url)
+          .subscribe(res => console.log(res))
+    }
 }
+
+  'O codigo de login com o google esta dando problema no CORS... como sempre!!!! kkkkkkkk'
