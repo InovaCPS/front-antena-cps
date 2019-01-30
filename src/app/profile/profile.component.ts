7238fc1a4a8b15a7a02bb9f7d74befc97e8232e2
RENAN LORENZETTI <lorenzetti2012@gmail.com>
2019-01-30 15:10:33 -0200

    Criação da pagina de perfil

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+  
+  bg = require('../../app/images/profile-bg.jpg')
+  profile = require('../../app/images/profile-pic.jpg')
+
+}
