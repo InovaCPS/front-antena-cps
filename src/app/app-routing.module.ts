87dc641f02df0de12a4d352e150f17dd4b5dfc41
Eduardo Simão <edu-simao@outlook.com>
2019-01-31 14:09:09 -0200

    Criação das routes e ajustes na estrutura do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { LandingComponent } from './landing-page/landing.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+      path: 'aluno',
+      component: ProfileComponent
+  }   
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:[
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
+
 export class AppRoutingModule { }
