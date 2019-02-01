a020e118d4a84a79b580ea7c019ba4f909d8789b
Eduardo Simão <edu-simao@outlook.com>
2019-02-01 17:39:01 -0200

    metodo para realizar o post de usuario na API do Bakc-End e ajustes no formulario de login

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { map } from "rxjs/operators"
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-profile',
@@ -9,4 +11,20 @@ export class CadastroComponent {
   
   logo = require('../../app/images/antena_cps.jpg')
   graf = require('../../app/images/background.png')
+  
+  apiRoot: string = "http://localhost:8080";
+  name: String;
+  Sname: String;
+  email: String;
+  senha: String;
+
+  constructor(private http: HttpClient) {}
+
+  doPOST() {
+    console.log("POST");
+    let url = `${this.apiRoot}/cp/parceiro`;
+    this.http
+      .post(url, { nome: this.name, sobrenome: this.Sname, email: this.email, senha: this.senha })
+      .subscribe(res => console.log("Cadastrado com Sucesso!!!"))         
+  }
 }
