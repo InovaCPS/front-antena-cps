49ad52431b33f677e25dbdf1fcfd3b591ec0d9dc
Daniel de Souza Rares <danielrares1@yahoo.com>
2019-02-25 15:51:08 -0300

    Implementação do Carousel

--- src/app/agenda/agenda.component.ts
+++ src/app/agenda/agenda.component.ts
@@ -1,7 +1,8 @@
 import 'fullcalendar';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalDismissReasons, NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface, SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
 import * as $ from 'jquery';
 
 
@@ -95,4 +96,113 @@ export class AgendaComponent implements OnInit {
             return `with: ${reason}`;
         }
     }
+
+
+    //SWIPER
+    @ViewChild(SwiperComponent) componentRef?: SwiperComponent;
+    @ViewChild(SwiperDirective) directiveRef?: SwiperDirective;
+
+    public show: boolean = true;
+    public slides = [
+        'Hackton',
+        'Palestra',
+        'Encontro',
+        'Reunião',
+        'Workshop',
+        'Desafio' 
+
+    ];
+
+    public type: string = 'component';
+
+    public disabled: boolean = false;
+
+    public config: SwiperConfigInterface = {
+        a11y: true,
+        direction: 'vertical',
+        slidesPerView: 1,
+        keyboard: true,
+        mousewheel: true,
+        scrollbar: false,
+        navigation: true,
+        pagination: false
+    };
+
+    private scrollbar: SwiperScrollbarInterface = {
+        el: '.swiper-scrollbar',
+        hide: false,
+        draggable: true
+    };
+
+    private pagination: SwiperPaginationInterface = {
+        el: '.swiper-pagination',
+        clickable: true,
+        hideOnClick: false
+    };
+
+
+
+    public toggleType(): void {
+        this.type = (this.type === 'component') ? 'directive' : 'component';
+    }
+
+    public toggleDisabled(): void {
+        this.disabled = !this.disabled;
+    }
+
+    public toggleDirection(): void {
+        this.config.direction = (this.config.direction === 'horizontal') ? 'vertical' : 'horizontal';
+    }
+
+    public toggleSlidesPerView(): void {
+        if (this.config.slidesPerView !== 1) {
+            this.config.slidesPerView = 1;
+        } else {
+            this.config.slidesPerView = 2;
+        }
+    }
+
+    public toggleOverlayControls(): void {
+        if (this.config.navigation) {
+            this.config.scrollbar = false;
+            this.config.navigation = false;
+
+            this.config.pagination = this.pagination;
+        } else if (this.config.pagination) {
+            this.config.navigation = false;
+            this.config.pagination = false;
+
+            this.config.scrollbar = this.scrollbar;
+        } else {
+            this.config.scrollbar = false;
+            this.config.pagination = false;
+
+            this.config.navigation = true;
+        }
+
+        if (this.type === 'directive' && this.directiveRef) {
+            this.directiveRef.setIndex(0);
+        } else if (this.type === 'component' && this.componentRef && this.componentRef.directiveRef) {
+            this.componentRef.directiveRef.setIndex(0);
+        }
+    }
+
+    public toggleKeyboardControl(): void {
+        this.config.keyboard = !this.config.keyboard;
+    }
+
+    public toggleMouseWheelControl(): void {
+        this.config.mousewheel = !this.config.mousewheel;
+    }
+
+    public onIndexChange(index: number): void {
+        console.log('Swiper index: ', index);
+    }
+
+    public onSwiperEvent(event: string): void {
+        console.log('Swiper event: ', event);
+    }
+
+
+
 }
