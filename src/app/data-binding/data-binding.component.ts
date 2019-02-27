6605bd95a51330463069b167207c308a79419929
Italo Carvalho <italo.998.carvalho@gmail.com>
2019-02-27 18:00:38 -0300

    Conexao da pagina de cadastro de projetos com a API back-end

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 })
 export class DataBindingComponent {
 
-    apiRoot: string = "http://antenacpsbackend-env.xryvsu2wzz.sa-east-1.elasticbeanstalk.com";
+    apiRoot: string = "http://localhost:8080";
 
     logo = require('../../app/images/antena_logo.png')
     usrn: String;
