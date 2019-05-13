80f5b22605bdee6216d0c804d7cc7ccffbfc23ad
Daniel Rares <danielrares1@yahoo.com>
2019-05-13 15:47:18 -0300

    inserção do vídeo

--- src/app/projeto/projeto.component.ts
+++ src/app/projeto/projeto.component.ts
@@ -125,3 +125,20 @@ export class ProjectComponent {
   viewCode = require('../../app/images/viewCode.png')
 
 }
+
+$(document).ready(function() {
+  var $videoSrc;  
+  $('.video-btn').click(function() {
+      $videoSrc = $(this).data( "src" );
+  });
+  console.log($videoSrc);
+  $('#myModal').on('shown.bs.modal', function (e) {
+  $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+  })
+  $('#myModal').on('hide.bs.modal', function (e) {
+      $("#video").attr('src',$videoSrc); 
+  }) 
+  });
+  
+  
+  
